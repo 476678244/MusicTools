a4d728f339d9d5719de582155f7588c0dfdf9198
zwu <zwu@coupang.com>
2020-03-06 16:09:16 +0800

    cd_bao

--- album_discovery/cd_bao.py
+++ album_discovery/cd_bao.py
@@ -1,32 +1,45 @@
 from os import listdir
 from os.path import isfile, join
 
-my_path = '/Volumes/WD1024G/Music/cdbao/100'
-found_albums = list()
-for f in listdir(my_path):
-    if (not isfile(join(my_path, f)) and not f.startswith('.')):
-        dir = join(my_path, f)
-        print(dir)
-        for album in listdir(dir):
-            if (not album.startswith('.')):
-                found_albums.append((join(dir, album), album))
-
-dest_dir = '/Volumes/WD1024G/Music/cdbao_union'
-test_album = found_albums[0]
-# import shutil
-#
-# shutil.copytree(test_album[0], join(dest_dir, test_album[1]))
-
 import tools.cp_with_prog as cp_with_prog
 import os
 
-for found_album in found_albums:
-    print(found_album)
-    os.mkdir(join(dest_dir, found_album))
-    for file in listdir(found_album[0]):
-        print(file)
-        cp_with_prog.copy(
-            join(found_album[0], file),
-            join(join(dest_dir, found_album[1]), file),
-            cp_with_prog.progress
-        )
+
+# test_album = found_albums[0]
+# import shutil
+# shutil.copytree(test_album[0], join(dest_dir, test_album[1]))
+
+def find_albums(input_path: str):
+    result = list()
+    for f in listdir(input_path):
+        if not isfile(join(input_path, f)) and not f.startswith('.'):
+            dir_path = join(input_path, f)
+            print(dir_path)
+            for album in listdir(dir_path):
+                if not album.startswith('.'):
+                    result.append((join(dir_path, album), album))
+    return result
+
+
+def copy_to_union(union_dir: str, albums: list):
+    for album in albums:
+        os.mkdir(join(union_dir, album[1]))
+        for file in listdir(album[0]):
+            print('Album: %s, File: %s' % (album, file))
+            print("Total Progress: %s "% "{0:.0%}".format(albums.index(album) / len(albums)))
+            cp_with_prog.copy(
+                join(album[0], file),
+                join(join(union_dir, album[1]), file),
+                cp_with_prog.progress
+            )
+
+
+copy_to_union(
+    '/Volumes/Backups.backupdb/Music/cdbao_union',
+    find_albums('/Volumes/WD1024G/Music/cdbao/100')
+)
+
+copy_to_union(
+    '/Volumes/Backups.backupdb/Music/cdbao_union',
+    find_albums('/Volumes/WD1024G/Music/cdbao/200')
+)
